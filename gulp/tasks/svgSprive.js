be9c133fc15bb615c6bc7bc276f711ebcf6f9724
@nikulin_konstantin <nik.v.62@mail.ru>
2022-05-19 16:00:54 +0500

    ref

--- gulp/tasks/svgSprive.js
+++ gulp/tasks/svgSprive.js
@@ -1,4 +1,6 @@
-import svgSprite from "gulp-svg-sprite"
+import svgSprite from "gulp-svg-sprite";
+import Cheerio from "cheerio";
+import replace from "gulp-replace";
 
 export const svgSprive = () => {
    return app.gulp.src(app.path.src.svgicons, {})
@@ -12,12 +14,11 @@ export const svgSprive = () => {
       .pipe(app.gulp.dest(app.path.build.svgicons))
       .pipe(svgSprite({
          mode: {
-            stack: {
+            symbol: {
                sprite: `../icons/svg-sprite.svg`,
                example: true
             }
          }
       }))
       .pipe(app.gulp.dest(app.path.build.images))
-
 }
